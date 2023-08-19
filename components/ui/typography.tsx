40da9d9e55f0a71cc28a2e223e5f14e99c2c4f02
KolliSai <saik98187@gmail.com>
2023-08-19 10:19:26 +0530

    added TypographyH1

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,5 +1,24 @@
 import { cn } from "@/lib/utils";
 
+export function TypographyH1({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1
+      className={cn(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
 export function TypographyH2({
   children,
   className,
